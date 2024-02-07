38a0ad670afca72f1aede6f7a846600e242526af
Rokitko Maksim <rokitkomaksim@gmail.com>
2024-02-07 17:01:50 +0200

    lesson 7

--- src/com/foretruff/jdbc/starter/JdbcRunner.java
+++ src/com/foretruff/jdbc/starter/JdbcRunner.java
@@ -2,23 +2,30 @@ package com.foretruff.jdbc.starter;
 
 import com.foretruff.jdbc.starter.util.ConnectionManager;
 
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
 public class JdbcRunner {
     public static void main(String[] args) throws SQLException {
         String sql = """
-                UPDATE info
-                SET data = 'TestTest'
-                WHERE id = 5
-                RETURNING *
+                SELECT *
+                FROM ticket
                 """;
         try (var connection = ConnectionManager.open();
-             var statement = connection.createStatement()) {
+             var statement = connection.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_UPDATABLE)) {
             System.out.println(connection.getSchema());
             System.out.println(connection.getTransactionIsolation());
-            var executeResult = statement.execute(sql);
-            System.out.println(executeResult);
+
+            var executeResult = statement.executeQuery(sql);
+            while (executeResult.next()) {
+                System.out.println(executeResult.getLong("id"));
+                System.out.println(executeResult.getString("passenger_no"));
+                System.out.println(executeResult.getBigDecimal("cost"));
+//                executeResult.previous();
+//                executeResult.afterLast();
+                System.out.println("----------------");
+            }
         }
     }
 
