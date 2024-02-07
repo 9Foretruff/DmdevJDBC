46ecc838ad3644cdbe8a5f4d7e8b80b54c03fdbb
Rokitko Maksim <rokitkomaksim@gmail.com>
2024-02-08 00:12:51 +0200

    lesson 18

---
+++ src/com/foretruff/jdbc/starter/dao/TicketDao.java
@@ -0,0 +1,15 @@
+package com.foretruff.jdbc.starter.dao;
+
+public class TicketDao {
+    private static final TicketDao INSTANCE = new TicketDao();
+
+    private TicketDao() {
+    }
+
+    public static TicketDao getInstance() {
+        return INSTANCE;
+    }
+
+
+
+}
