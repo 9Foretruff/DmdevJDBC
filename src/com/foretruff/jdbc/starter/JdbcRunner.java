5efa1b89408e3b8d0aa5facc57e14e104e1ef98d
Rokitko Maksim <rokitkomaksim@gmail.com>
2024-02-07 16:35:10 +0200

    lesson 5

--- src/com/foretruff/jdbc/starter/JdbcRunner.java
+++ src/com/foretruff/jdbc/starter/JdbcRunner.java
@@ -3,11 +3,26 @@ package com.foretruff.jdbc.starter;
 import com.foretruff.jdbc.starter.util.ConnectionManager;
 
 import java.sql.SQLException;
+import java.sql.Statement;
 
 public class JdbcRunner {
     public static void main(String[] args) throws SQLException {
-        try (var connection = ConnectionManager.open()) {
+        String sql = """
+                CREATE TABLE IF NOT EXISTS info
+                (
+                    id SERIAL PRIMARY KEY ,
+                    data TEXT NOT NULL
+                );
+                """;
+        try (var connection = ConnectionManager.open();
+             var statement = connection.createStatement()) {
+
+            System.out.println(connection.getSchema());
             System.out.println(connection.getTransactionIsolation());
+
+            var executeResult = statement.execute(sql);
+
+            System.out.println(executeResult);
         }
     }
 
